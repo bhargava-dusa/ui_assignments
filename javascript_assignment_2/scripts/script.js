705ffe6e3417076c5e0053b152ad9bd1ebac1a48
Bhargava Chandra Dusa <80024894+bhargava-dusa@users.noreply.github.com>
2021-04-05 17:29:43 +0530

    Update script.js

--- javascript_assignment_2/scripts/script.js
+++ javascript_assignment_2/scripts/script.js
@@ -2,7 +2,6 @@ function onDragStart(event) {
     event
       .dataTransfer
       .setData('text/plain', event.target.id);
-    console.info("dragging id:" + event.target.id);
 }
 
 function onDragOver(event) {
@@ -14,17 +13,10 @@ function onDrop(event) {
     const item = document.getElementById(id).innerText;
     const tableId = event.target.id;
     const table = document.getElementById(tableId);
-    console.info("table id: " + tableId);
     const parentElement = table.parentElement;
-    console.info("parentElement.innerHTML: " + parentElement.innerHTML);
-    console.info("parentElement.children.length: " + parentElement.children.length);
     table.parentElement.children[1].innerHTML += "<li class='list-group-item'>"+item+"</li>";
     let itemCount = table.children[0].children[0].innerText;
     table.children[0].children[0].innerText = parseInt(itemCount) + 1;
     table.children[0].style.display = 'block';
     event.dataTransfer.clearData();
-}
-
-function deleteItem(tableId, itemName){
-    console.info(tableId + ", " + itemName);
 }
